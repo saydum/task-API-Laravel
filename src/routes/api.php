52c9e17235f3d710ad80afd73671e87b298156bf
Saydum Halibekov <saydum.it5@gmail.com>
2022-05-27 11:49:08 +0300

    Created API route Notebook

--- src/routes/api.php
+++ src/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\NotebookController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('all', [NotebookController::class, ''])->name('notebook.all');
+Route::post('add', [NotebookController::class, ''])->name('notebook.add');
+Route::get('get/{Notebook}', [NotebookController::class, ''])->name('notebook.get');
+Route::put('update/{Notebook}', [NotebookController::class, ''])->name('notebook.update');
+Route::delete('delete/{Notebook}', [NotebookController::class, ''])->name('notebook.delete');
